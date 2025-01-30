f9a69fc4b734ef4985d268fa7ad6bdfadf9f8e74
Estefanievz <23610289@utgz.edu.mx>
2025-01-30 10:43:54 -0600

    hola

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListasComponent } from './listas/listas.component';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { ListaService } from '../services/lista.service';
 
 
 
 @NgModule({
   declarations: [ListasComponent],
   imports: [
+    IonicModule,
+    FormsModule,
     CommonModule
-  ]
+  ],
+  exports:[ListasComponent]
 })
-export class ComponentsModule { }
+export class ComponentsModule {
+  constructor(public listaService:ListaService){
+    
+  }
+ }
